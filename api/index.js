622101b7874a8bbc8b0a97575f21eb8557eeb164
Mauricio Gomes <mauriciogomes.fonseca@outlook.com>
2026-05-05 11:21:17 -0300

    fix: corrigir nome da variavel SUPABASE_SERVICE_ROLE_KEY

--- api/index.js
+++ api/index.js
@@ -9,8 +9,9 @@ const sugestoes        = require('../lib/handlers/sugestoes.js');
 const totem            = require('../lib/handlers/totem.js');
 const usuarios         = require('../lib/handlers/usuarios.js');
 const vagas            = require('../lib/handlers/vagas.js');
-const agenteConfirmar  = require('../lib/handlers/agente/confirmar.js');
-const agenteFila       = require('../lib/handlers/agente/fila.js');
+const agenteConfirmar    = require('../lib/handlers/agente/confirmar.js');
+const agenteFila         = require('../lib/handlers/agente/fila.js');
+const agenteSalvarPerson = require('../lib/handlers/agente/salvar-person-id.js');
 
 const rotas = {
   '/api/acessos-especiais': acessosEspeciais,
@@ -24,8 +25,9 @@ const rotas = {
   '/api/totem':             totem,
   '/api/usuarios':          usuarios,
   '/api/vagas':             vagas,
-  '/api/agente/confirmar':  agenteConfirmar,
-  '/api/agente/fila':       agenteFila,
+  '/api/agente/confirmar':       agenteConfirmar,
+  '/api/agente/fila':            agenteFila,
+  '/api/agente/salvar-person-id': agenteSalvarPerson,
 };
 
 module.exports = function handler(req, res) {
