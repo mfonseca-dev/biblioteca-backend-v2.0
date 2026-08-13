7930364a4862aeccf62f647bfd1bd6b7b382475f
Mauricio Gomes <mauriciogomes.fonseca@outlook.com>
2026-08-13 19:27:52 -0300

    chore: remove projeto antigo biblioteca-paiva-netto (substituido por v2)

--- biblioteca-paiva-netto/api/debug.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    url: process.env.SUPABASE_URL,
-    hasKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY
-  });
-}
--- biblioteca-paiva-netto/hash.js
+++
@@ -1,2 +0,0 @@
-const b=require('bcryptjs');
-console.log(b.hashSync('Paiva2008',10));
